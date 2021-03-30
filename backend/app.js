2b9b7a6cf4b9be4cd12c5a8810406999e3569f03
tombetthauser <tombetthauser@gmail.com>
2021-03-30 15:36:08 -0700

    Get error handlers working

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
+const { ValidationError } = require('sequelize');
+
 const routes = require('./routes');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -37,6 +39,37 @@ app.use(
   })
 );
 
-app.use(routes);
+app.use(routes); // Connect all the routes
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("I tried super hard but I just couldn't find the droids you were looking for...");
+  err.title = "Resource Not Found...";
+  err.errors = ["I tried super hard but I just couldn't find the droids you were looking for..."];
+  err.status = 420;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'A sequelize ValidationError just totally occurred...';
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 module.exports = app;
