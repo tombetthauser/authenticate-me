f1cac72afe6bf60eaaade220aa1335b270421a07
tombetthauser <tombetthauser@gmail.com>
2021-03-31 21:15:29 -0700

    Comment out hello world test route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,9 +5,10 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function (req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+// router.get('/hello/world', function (req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.cookie('toms-cookie', "bananahana");
+//   res.send('Hello World!');
+// });
 
 module.exports = router;
